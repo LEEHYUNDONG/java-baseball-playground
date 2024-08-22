39277a6d478e69778297d748f9d3dfeeec4934ce
easttwave <hd.lee6@cj.net>
2024-08-22 13:29:26 +0900

    feat: referee 기능 구현 - 심판으로 점수와 출력 담당

---
+++ src/main/java/baseball/Referee.java
@@ -0,0 +1,48 @@
+package baseball;
+
+import javafx.util.Pair;
+
+/**
+ * @author : lhd
+ * @packageName : baseball
+ * @fileName : java-baseball-playground
+ * @description :
+ * <p>
+ * ====================================================
+ * DATE            AUTHOR              NOTE
+ * ====================================================
+ * 2024-08-22         lhd
+ */
+public class Referee {
+
+
+    public static Pair<Integer, Integer> compare(String computerValue, String playerValue) {
+        int strike = 0;
+        int ball = 0;
+
+        // strike
+        for (int i = 0; i < 3; i++) {
+            if (computerValue.charAt(i) == playerValue.charAt(i)) {
+                strike++;
+            } else if(computerValue.contains(String.valueOf(playerValue.charAt(i)))) {
+                ball++;
+            }
+        }
+        return new Pair<>(strike, ball);
+    }
+
+    public static String printScore(int strike, int ball) {
+        if(strike > 0 && ball > 0) {
+            return String.format("%d볼 %d스트라이크",ball, strike);
+        } else if(strike > 0) {
+            return String.format("%d스트라이크", strike);
+        } else if(ball > 0) {
+            return String.format("%d볼", ball);
+        }
+        return "낫싱";
+    }
+
+    public static void print(int strike, int ball) {
+        System.out.println(printScore(strike, ball));
+    }
+}
