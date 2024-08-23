0ba86f71024750efab223856eb05b5131892c94a
easttwave <hd.lee6@cj.net>
2024-08-23 13:45:10 +0900

    feat: 숫자 검증 utils 개발

---
+++ src/test/java/baseball/v2/NumberValidationTest.java
@@ -0,0 +1,25 @@
+package baseball.v2;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author : lhd
+ * @packageName : baseball.v2
+ * @fileName : java-baseball-playground
+ * @description :
+ * <p>
+ * ====================================================
+ * DATE            AUTHOR              NOTE
+ * ====================================================
+ * 2024-08-23         lhd
+ */
+public class NumberValidationTest {
+
+    @Test
+    void validateTheNumberIsBetween0And9() {
+        assertThat(NumberValidationUtils.validate(1)).isTrue();
+        assertThat(NumberValidationUtils.validate(9)).isTrue();
+    }
+}
